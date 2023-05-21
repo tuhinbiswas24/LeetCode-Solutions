40570886481670cdf8429285aa4e73bc02999e58
tuhinbiswas24 <120563029+tuhinbiswas24@users.noreply.github.com>
2023-05-21 19:00:29 +0530

    Create Roman to Integer.cpp

---
+++ C++  Solutions/Roman to Integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(i>0 && mp[s[i]]>mp[s[i-1]])
+                ans+=mp[s[i]]-2*mp[s[i-1]];
+            else
+                ans+=mp[s[i]];    
+        }
+        return ans;
+    }
+};
