66adaf911a6872676e78251ebdad97a63cb68438
tuhinbiswas24 <120563029+tuhinbiswas24@users.noreply.github.com>
2023-05-25 08:58:27 +0530

    Create Lexicographically Smallest Palindrome.cpp

---
+++ C++  Solutions/Lexicographically Smallest Palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int n = s.size();
+        for (int i=0;i<n/2;i++) {
+            if (s[i]!= s[n-i-1]) {
+                if (s[i]<s[n-i-1]) {
+                    s[n-i-1]=s[i];
+                } 
+                else {
+                    s[i]=s[n-i-1];
+                }
+            }
+        }
+        return s;
+    }
+};
